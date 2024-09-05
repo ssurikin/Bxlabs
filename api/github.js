363c62396aa2cde1e087640090619f2cbdb739ca
ssurikin <www.nikita.alekseev2002@gmail.com>
2024-09-05 22:47:46 +0300

    Update github.js

--- api/github.js
+++ api/github.js
@@ -14,12 +14,3 @@ export default async (req, res) => {
         res.status(500).json({ error: 'Error fetching data' });
     }
 };
-// Пример серверной функции на Node.js
-export default async function handler(req, res) {
-    const data = await fetchData(); // Функция для получения данных
-
-    // Логирование размера данных
-    console.log(`Размер данных: ${Buffer.byteLength(JSON.stringify(data), 'utf8')} байт`);
-
-    res.status(200).json(data);
-}
