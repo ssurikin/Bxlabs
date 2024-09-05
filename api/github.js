24dea728a5e757d0f907a5c9028d5c5420534a1a
ssurikin <www.nikita.alekseev2002@gmail.com>
2024-09-05 23:42:31 +0300

    Update github.js

--- api/github.js
+++ api/github.js
@@ -1,6 +1,6 @@
 // api/github.js
 export default async (req, res) => {
-const token = process.env.GITHUB_TOKEN;
+    const token = process.env.GITHUB_TOKEN; // Используйте переменные среды для токена
 
     try {
         const response = await fetch(`https://api.github.com${req.url}`, {
@@ -8,9 +8,15 @@ const token = process.env.GITHUB_TOKEN;
                 'Authorization': `token ${token}`
             }
         });
+
+        if (!response.ok) {
+            throw new Error(`GitHub API responded with status ${response.status}`);
+        }
+
         const data = await response.json();
         res.status(200).json(data);
     } catch (error) {
-        res.status(500).json({ error: 'Error fetching data' });
+        console.error('Ошибка при запросе к GitHub API:', error);
+        res.status(500).json({ error: 'Ошибка при запросе к GitHub API' });
     }
 };
